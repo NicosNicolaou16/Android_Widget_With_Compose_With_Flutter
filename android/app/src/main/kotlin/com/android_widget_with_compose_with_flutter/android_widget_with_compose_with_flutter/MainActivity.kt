313ea981a7e4b2b07f3838e466dadfade38d1d34
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-02-18 14:54:26 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/android_widget_with_compose_with_flutter/android_widget_with_compose_with_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.android_widget_with_compose_with_flutter.android_widget_with_compose_with_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
